0d8bd34e8b45e209c79078f3a66bcb219fd7f070
Martin Klauer <mdklauer@gmail.com>
2019-10-02 10:38:42 +0200

    add files

---
+++ index.js
@@ -0,0 +1,7 @@
+const chalk = require('chalk')
+
+console.log(chalk.blue('Elise'));
+console.log(chalk.red('Aurélia'));
+console.log(chalk.green('Baptiste'));
+console.log(chalk.red.bgWhite('Titouan'));
+console.log(chalk.yellow.bold('Martin'));
